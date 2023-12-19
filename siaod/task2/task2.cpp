38a50ad1597312336421938dedf2dc429a9c0e3d
Oleg <oleg-kruglenko@mail.ru>
2023-12-19 20:21:13 +0300

    Cafe create

--- siaod/task2/task2.cpp
+++ siaod/task2/task2.cpp
@@ -6,25 +6,27 @@ using namespace std;
 
 struct License
 {
-    //licenseNumber;
-    //char licenseName[50];
+    int licenseNumber;
+    char licenseName[50];
     string licenseFounder;
-    //bool licenseStatus;
+    bool licenseStatus;
 };
 
 int main()
 {
-    License read1 = {"f"};
+    string fileName;
 
-    fstream output_file("license.bin", ios::binary | ios::in | ios::out);
+    cout << "Input file name: ";
+    cin >> fileName;
+
+    fstream output_file("license.bin");
     if (!output_file.is_open())
     {
         cout << "File is not open!" << endl;
         return 1;
     }
 
-    output_file.seekp(0 * sizeof(License));
-    output_file.write(reinterpret_cast<char*>(&read1), sizeof(read1));
+    output_file << "Hello!";
     output_file.close();
 
     cout << "programm is finish!";
